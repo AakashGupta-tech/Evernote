76e515a5ec543b6329e2197abcc461d63a640866
Aakash13 <ag98krishna@gmail.com>
2020-08-15 11:51:17 +0530

    Updates

---
+++ precache-manifest.2362f0ac918b871f105e7ecd89b2cd1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ca6be6b69075aa2a07783d34637c6e",
+    "url": "/Evernote/index.html"
+  },
+  {
+    "revision": "0e6d00be6450d68dee75",
+    "url": "/Evernote/static/css/main.65553a91.chunk.css"
+  },
+  {
+    "revision": "febc1ebe6617b4a69a02",
+    "url": "/Evernote/static/js/2.c155e7ab.chunk.js"
+  },
+  {
+    "revision": "0e6d00be6450d68dee75",
+    "url": "/Evernote/static/js/main.3ade8a2f.chunk.js"
+  },
+  {
+    "revision": "19342f7aa4ed99cbcef1",
+    "url": "/Evernote/static/js/runtime~main.b916a788.js"
+  }
+]);
